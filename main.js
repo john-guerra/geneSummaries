7890dbd5308ef7f22419bbf537d5f2b86e35e9e7
Meili Vanegas Hernández <m.vanegas10@uniandes.edu.co>
2017-04-05 08:55:45 -0500

    Added icicle

--- main.js
+++ main.js
@@ -22,7 +22,22 @@ var fmt = function (d) { return d; };
 	//   if (err) throw err;
 
 function computeTree(data) {
-	return new CSVtoTree("/", "NAME", "SCORE").getTreeWithHierarchy(data, ["NAME", "ORGANISM", "SCORE", "CODE", "TAXID", "family", "species", "order", "genus", "class"].reverse());
+	return new CSVtoTree("/", "NAME", "SCORE").getTreeWithHierarchy(data, ["SCORE", "NAME", "ORGANISM", "species", "genus","family", "order", "class"].reverse());
+}
+
+function createTree(data) {
+	var tree = {};
+	data.forEach(function(d){
+		if(!tree[d.class]) tree[d.class] = {};
+		if(!tree[d.class][d.order]) tree[d.class][d.order] = {};
+		if(!tree[d.class][d.order][d.family]) tree[d.class][d.order][d.family] = {};
+		if(!tree[d.class][d.order][d.family]) tree[d.class][d.order][d.family] = {};
+		if(!tree[d.class][d.order][d.family][d.genus]) tree[d.class][d.order][d.family][d.genus] = {};
+		if(!tree[d.class][d.order][d.family][d.genus][d.species]) tree[d.class][d.order][d.family][d.genus][d.species] = {};
+		if(!tree[d.class][d.order][d.family][d.genus][d.species][d.ORGANISM]) tree[d.class][d.order][d.family][d.genus][d.species][d.ORGANISM] = {};
+		if(!tree[d.class][d.order][d.family][d.genus][d.species][d.ORGANISM][d.NAME]) tree[d.class][d.order][d.family][d.genus][d.species][d.ORGANISM][d.NAME] = d.SCORE;
+	});
+	return tree;
 }
 
 function redraw() {
@@ -47,8 +62,6 @@ function redraw() {
 
 	var attribute = d3.select("#attribute").property("value");	
 	var typeCount = d3.select("#typeCount").property("value");
-
-	console.log(typeCount);	
 
 	var dCounts = {};	
 
@@ -83,8 +96,76 @@ function redraw() {
 	});
 
 	redrawTable(dCountsArr,["text","count"]);
+	redrawIcicle(createTree(data));
 	redrawWordCloud(data,dCounts);
 	redrawTree(data);
+}
+
+function redrawIcicle(root) {
+	var width = 960,
+    height = 500;
+
+	var x = d3v4.scaleLinear()
+	    .range([0, width]);
+
+	var y = d3v4.scaleLinear()
+	    .range([0, height]);
+
+	var color = d3v4.scaleOrdinal(d3v4.schemeCategory20c);
+
+	var partition = d3v4.partition()
+	    .size([width, height])
+	    .padding(0)
+	    .round(true);
+	
+	var svg = d3v4.select("#icicle").append("svg")
+	    .attr("width", width)
+	    .attr("height", height);
+
+	console.log(root);
+	root = d3v4.hierarchy(d3v4.entries(root)[0], function(d) {
+	  return d3v4.entries(d.value)
+	})
+	.sum(function(d) { return d.value })
+	.sort(function(a, b) { return b.value - a.value; });
+	console.log(root.descendants())
+	partition(root);
+
+	var bar = svg.selectAll("g")
+	  	.data(root.descendants())
+		.enter().append("g");
+	
+	var rect = bar.append("rect")
+		.attr("x", function(d) { return d.x0; })
+	  	.attr("y", function(d) { return d.y0; })
+	  	.attr("width", function(d) { return d.x1 - d.x0; })
+	  	.attr("height", function(d) { return d.y1 - d.y0; })
+	  	.attr("fill", function(d) { return color((d.children ? d : d.parent).data.key); })
+	  	.on("click", clicked);
+	  
+	var text = bar.append("text")
+		.attr("x", function(d) { return d.x0 + (d.x1 - d.x0)/2; })
+	  	.attr("y", function(d) { return d.y0 + (d.y1 - d.y0)/2; })
+      	.attr("dy", ".35em")
+		.text(function (d) { return d.data.key});
+
+	function clicked(d) {
+	  	x.domain([d.x0, d.x1]);
+	  	y.domain([d.y0, height]).range([d.depth ? 20 : 0, height]);
+
+	  	rect.transition()
+			.duration(750)
+			.attr("x", function(d) { return x(d.x0); })
+			.attr("y", function(d) { return y(d.y0); })
+			.attr("width", function(d) { return x(d.x1) - x(d.x0); })
+			.attr("height", function(d) { return y(d.y1) - y(d.y0); });
+
+	  	text.transition()
+			.duration(750)
+			.attr("x", function(d) { return x(d.x0 + (d.x1 - d.x0)/2); })
+			.attr("y", function(d) { return y(d.y0 + (d.y1 - d.y0)/2); });
+	}
+
 }
 
 function redrawTree(data) {
@@ -127,12 +208,9 @@ function redrawTable (mData,columns) {
 	.enter()
 	.append('tr');
 
-	console.log(rows);
-
 	// create a cell in each row for each column
 	var cells = rows.selectAll('td')
 	.data(function (row) {
-		console.log(Object.keys(row));
 		return columns.map(function (column) {
 			return {column: column, value: row[column]};
 		});
